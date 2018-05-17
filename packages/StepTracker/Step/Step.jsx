7c7d795062696514d722750d9df4e7ebe78a3ea2
marcod1419 <marcod1419@gmail.com>
2018-05-17 15:21:44 -0400

    refactor(core-step-tracker): code cleanup
    Improved application of styles in Step
    Removed some unneeded eslint ignores
    Removed Step prop descriptions
    Improved generation of Steps in StepTracker

--- packages/StepTracker/Step/Step.jsx
+++ packages/StepTracker/Step/Step.jsx
@@ -10,56 +10,32 @@ import styles from '../StepTracker.modules.scss'
  */
 
 const Step = ({ label, status, stepNumber, stepIndex }) => {
-  const cls = [styles.step]
+  let cls
   if (status > stepIndex) {
-    cls.push(styles.completed)
+    cls = styles.completed
   } else if (status === stepIndex) {
-    cls.push(styles.processing)
+    cls = styles.processing
+  } else {
+    cls = styles.step
   }
 
-  /* eslint-disable jsx-a11y/aria-props */
   return (
-    <li
-      className={cls.join(' ')}
-      aria-label={label}
-      aria-current={status === stepIndex ? 'true' : 'false'}
-    >
+    <li className={cls} aria-label={label} aria-current={status === stepIndex ? 'true' : 'false'}>
       <span className={styles.icon}>
-        <span style={{ opacity: status > stepIndex ? 1 : 0 }}>
-          <Icon symbol="checkmark" size={16} variant="inverted" />
-        </span>
+        {status > stepIndex ? <Icon symbol="checkmark" size={16} variant="inverted" /> : <br />}
       </span>
       <span className={styles.label}>
         {stepNumber}. {label}
       </span>
     </li>
   )
-  /* eslint-enable jsx-a11y/aria-props */
 }
 
 Step.propTypes = {
-  /**
-   * The label of this step.
-   */
-  label: PropTypes.string,
-  /**
-   * The current step the user is on.
-   *
-   * @ignore
-   */
-  status: PropTypes.number, // eslint-disable-line react/require-default-props
-  /**
-   * The real, user-friendly step number. This is the stepIndex + 1.
-   *
-   * @ignore
-   */
-  stepNumber: PropTypes.number, // eslint-disable-line react/require-default-props
-  /**
-   * The step's index. This represents its place in the steps array of the StepTracker component.
-   *
-   * @ignore
-   */
-  stepIndex: PropTypes.number, // eslint-disable-line react/require-default-props
+  label: PropTypes.string.isRequired,
+  status: PropTypes.number.isRequired,
+  stepNumber: PropTypes.number.isRequired,
+  stepIndex: PropTypes.number.isRequired,
 }
 
 export default Step
--- packages/StepTracker/StepTracker.jsx
+++ packages/StepTracker/StepTracker.jsx
@@ -11,30 +11,30 @@ import styles from './StepTracker.modules.scss'
  * @version ./package.json
  */
 
-const StepTracker = props => {
+const StepTracker = ({ current, steps, stepText, outOfText }) => {
   return (
     <div>
       <ul className={styles.container}>
-        {props.steps.map((label, index) => {
-          return React.createElement(StepTracker.Step, {
-            label,
-            status: props.current,
-            label: props.steps[index], // eslint-disable-line no-dupe-keys
-            stepNumber: index + 1,
-            stepIndex: index,
-            key: index, // eslint-disable-line react/no-array-index-key
-          })
+        {steps.map((label, index) => {
+          return (
+            /* eslint-disable react/no-array-index-key */
+            <StepTracker.Step
+              status={current}
+              label={steps[index]}
+              stepNumber={index + 1}
+              stepIndex={index}
+              key={index}
+            />
+            /* eslint-enable react/no-array-index-key */
+          )
         })}
       </ul>
       <div className={styles.mobileLabel}>
         <span className={styles.mobileLabelStepInfo}>
-          {props.stepText}{' '}
-          {props.current < props.steps.length ? props.current + 1 : props.steps.length}{' '}
-          {props.outOfText} {props.steps.length}:{' '}
+          {stepText} {current < steps.length ? current + 1 : steps.length} {outOfText}{' '}
+          {steps.length}:{' '}
         </span>
-        {props.current < props.steps.length
-          ? props.steps[props.current]
-          : props.steps[props.steps.length - 1]}
+        {current < steps.length ? steps[current] : steps[steps.length - 1]}
       </div>
     </div>
   )
